1bfa63ee9ef42e6d95def3a7b356c8503ff28973
francescroy <francescroy@gmail.com>
2021-10-27 14:28:06 +0200

    mmmm

--- client/config.py
+++ client/config.py
@@ -1,5 +1,5 @@
 DEFAULT_SERVER_URL = 'http://127.0.0.1:5000'
 GLOBAL_TMP_PATH = '/tmp'
-GLOBAL_DATASETS = '/home/francesc/Escritorio/fed_learning' # inside there is chest_xray folder
+GLOBAL_DATASETS = '/home/francesc/Escritorio/folder_client' # inside there is chest_xray folder
 #GLOBAL_DATASETS = '/home/francesc/Escritorio/datasets' # inside there is chest_xray folder
-INITIAL_MODEL_PATH = '/home/francesc/Escritorio/fed_learning/keras_model'
+INITIAL_MODEL_PATH = '/home/francesc/Escritorio/folder_client/keras_model'
--- up_client.sh
+++ up_client.sh
@@ -3,6 +3,6 @@
 #export CLIENT_URL='http://localhost:5001'
 #flask run --port 5001
 
-sudo docker run --rm --name fl-client -e CLIENT_URL='http://172.17.0.3:5000' -e SERVER_URL='http://172.17.0.2:5000' -v /home/francesc/Escritorio/datasets:/federated-learning-network/datasets -v /home/francesc/Escritorio/initial_models:/federated-learning-network/initial_models fl-client:latest
+#sudo docker run --rm --name fl-client -e CLIENT_URL='http://172.17.0.3:5000' -e SERVER_URL='http://172.17.0.2:5000' -v /home/francesc/Escritorio/datasets:/federated-learning-network/datasets -v /home/francesc/Escritorio/initial_models:/federated-learning-network/initial_models fl-client:latest
 
-sudo docker run --rm --name fl-client -p 5000:5000 -e CLIENT_URL='http://10.228.207.201:5000' -e SERVER_URL='http://10.139.40.19:5000' -v /home/francesc/Escritorio/datasets:/federated-learning-network/datasets -v /home/francesc/Escritorio/initial_models:/federated-learning-network/initial_models fl-client:latest
+sudo docker run --rm --name fl-client -p 5000:5000 -e CLIENT_URL='http://10.228.207.201:5000' -e SERVER_URL='http://10.139.40.19:5000' -v /home/francesc/Escritorio/folder_client:/federated-learning-network/datasets_and_model fl-client:latest
